406ee4352cf7319b094cf9422732b36c8ca2dab3
honglihongli <s_hong.li@ousd.org>
2020-03-05 02:17:20 +0000

    What did you

--- js/script.js
+++ js/script.js
@@ -6,17 +6,22 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
-
 $(".onlybutton").click(function() {
+    $(".imageplace").empty();
     var response = $(".type").val();
-    fetch("https://api.giphy.com/v1/gifs/search?q="+response+"&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY")
+    fetch(`https://api.giphy.com/v1/gifs/search?q=`+response+`&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY`)
       .then(function(gif) {
         return gif.json();
       })
-      .then(function(response) {
-        console.log(response);
-        var image = response.data[2];
-        console.log(image);
-        $(".imageplace").append(`<img src = ${image}>`);
+      .then(function(data) {
+        var first = data.data[(Math.floor((Math.random() * data.data.length)))].images.downsized_large.url
+        var gifme =`<img src="`+first+`">`;
+        $(".imageplace").append(gifme);
+        $(".mail").click(function() {
+  var topic = $(".gif10").val();
+  var user = $(".picture").val();
+  var response = $(".type").val();
+  window.open("mailto:"+topic+"?Subject="+user+"&body="+first);
+})
       })
 });
